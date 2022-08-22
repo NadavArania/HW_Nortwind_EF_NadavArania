8fb3a06aa56d9ef1160f3007fb2c5b3ec46dc954
NadavArania <97458713+NadavArania@users.noreply.github.com>
2022-08-22 18:42:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using HW_EF6_NadavArania.Models;
+using System.Text.Json;
+
+namespace HW_EF6_NadavArania
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new NorthwindContext())
+            {
+                //Targil 1
+                var listRegions = db.Regions.ToList();
+
+                //Targil 2
+                listRegions.ForEach(x => Console.WriteLine($"Region Descrip : {x.RegionDescription}"));
+
+                //Targil 3
+                var listTerritories = db.Territories.ToList();
+
+                //Targil 4
+                listTerritories.ForEach(x => Console.WriteLine($"Territory Descrip : {x.TerritoryDescription}"));
+
+                // Targil 5
+                foreach (var region in listRegions)
+                {
+                    Console.WriteLine(region.RegionDescription);
+                    foreach (var territory in listTerritories)
+                    {
+                        Console.WriteLine("\n -" + territory.TerritoryDescription);
+                    }
+                };
+            }
+        }
+    }
+}
